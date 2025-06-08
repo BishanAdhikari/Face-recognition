54eb52f5a556f1e07ec38f35bbf506428a942b02
BishanAdhikari <bishanadhikari.111@gmail.com>
2025-06-08 22:16:06 +0545

    Add TripletDataset class to load anchor, positive, negative image samples

--- 2.py
+++ 2.py
@@ -7,3 +7,53 @@ import numpy as np
 import random
 import os
 from PIL import Image
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
+])
+
+class TripletDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.data = []
+        self.labels = []
+        self.persons = os.listdir(root_dir)
+
+        for person in self.persons:
+            images = os.listdir(os.path.join(root_dir, person))
+            if len(images) < 2:
+                continue  # Ignore folders with less than 2 images
+            self.data.append(images)
+            self.labels.append(person)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        anchor_label = self.labels[index]
+        anchor_dir = os.path.join(self.root_dir, anchor_label)
+        anchor_img = random.choice(os.listdir(anchor_dir))
+        anchor_path = os.path.join(anchor_dir, anchor_img)
+
+        positive_img = random.choice(os.listdir(anchor_dir))
+        positive_path = os.path.join(anchor_dir, positive_img)
+
+        negative_label = random.choice([x for x in self.labels if x != anchor_label])
+        negative_dir = os.path.join(self.root_dir, negative_label)
+        negative_img = random.choice(os.listdir(negative_dir))
+        negative_path = os.path.join(negative_dir, negative_img)
+
+        anchor = Image.open(anchor_path).convert("RGB")
+        positive = Image.open(positive_path).convert("RGB")
+        negative = Image.open(negative_path).convert("RGB")
+
+        if self.transform:
+            anchor = self.transform(anchor)
+            positive = self.transform(positive)
+            negative = self.transform(negative)
+
+        return anchor, positive, negative
+
